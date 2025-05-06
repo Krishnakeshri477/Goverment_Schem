d6fb8128d0a45b4eeec0a0c554602284fe7aa029
HARSH RAJ <154910680+harshrj00@users.noreply.github.com>
2025-05-06 05:30:05 +0530

    enter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Navbar from './Navbar/Navbar'
-import {Route,Routes} from 'react-router'
 import Home from './Pages/Home'
-import Context from './Pages/Context'
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Home/>
     </div>
   )
 }
